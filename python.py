8d6d29e8e677e16d622b29484b902e2412295888
raffikss <rafaelsdmitrijevs@gmail.com>
2024-11-04 10:03:29 +0200

    Added more comments to python.py

--- python.py
+++ python.py
@@ -1,12 +1,12 @@
 from turtle import *
-
+#Configuration settings for turtle
 turtle = Turtle()
 turtle.color('green', 'red')
 turtle.speed(50)
 
 def draw_star(n, size):
     if n % 2 == 0:
-        print(" No odd number for n.")
+        print(" No odd number for n.") # This function calculates the stars points, and doesn't let it be an odd number of points
         return
     
     angle = 180 - (180 / n) 
@@ -17,17 +17,17 @@ def draw_star(n, size):
 
 
 turtle.penup()
-turtle.goto(-40, 0)
+turtle.goto(-40, 0)  #Goes to the start coordinates
 turtle.pendown()
-draw_star(19, 150)
+draw_star(19, 150) # Makes the star - parameters are given in the brackets
 
 
 turtle.penup()
-turtle.goto(100, 0)  
+turtle.goto(100, 0)   #Goes to the start coordinates
 turtle.pendown()
-draw_star(51, 150)
+draw_star(51, 150) # Makes the star - parameters are given in the brackets
 
-turtle.hideturtle()
+turtle.hideturtle() #Hides the turtle
 done()
 
 #something
