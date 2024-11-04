3912b2ae40804fa20a57c85eb9f6543210a66540
raffikss <rafaelsdmitrijevs@gmail.com>
2024-11-04 10:05:58 +0200

    Added comments to randomassignment.py

--- randomassignment.py
+++ randomassignment.py
@@ -10,12 +10,12 @@ print(attalums)
 
 
 if km >= h:
-    print('parak talu jaiet')
+    print('parak talu jaiet') # If the km are more or equal than hours it prints out the text
 
 if km<h:
-    print('Varu aiziet tik talu')
+    print('Varu aiziet tik talu') # If the km are less than hours it prints out the text
 
 if attalums > 50:
-    print('ar kajam neaiziet')
+    print('ar kajam neaiziet') #If the distance is bigger than 50 it prints out the said result
 else:
-    print ('var aiziet')
+    print ('var aiziet') #If its smaller than 50, it prints out this result
