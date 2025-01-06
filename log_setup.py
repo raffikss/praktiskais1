15446a1bfacfb4953139dc1693e9b6a18e4f3210
raffikss <rafaelsdmitrijevs@gmail.com>
2025-01-06 12:31:52 +0200

    Update log_setup.py

--- log_setup.py
+++ log_setup.py
@@ -8,7 +8,7 @@ log_file = 'logs.log'
 file_handler = logging.FileHandler(log_file, encoding='utf-8')
 file_handler.setLevel(logging.DEBUG)
 #Formating logging file formater?
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(message)s')
 file_handler.setFormatter(formatter)
 #Adding logging file to the logger
 logger.addHandler(file_handler)
